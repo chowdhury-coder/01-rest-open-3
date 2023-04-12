1fdbf1c947dbb3bf0894120214b3d347360cf8e3
Kat Law <kathleen.law@multiverse.io>
2023-04-12 16:39:22 -0400

    init

--- index.js
+++ index.js
@@ -8,48 +8,27 @@ const { Op } = require('sequelize');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.post('/dogs', async (req, res, next) => {
+app.get('', async (req, res, next) => {
   try {
-    const {name, breed, color, description} = req.body;
-    const dog = await Dog.create({name, breed, color, description});
-    res.send(dog);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.delete('/dogs/:id', async (req, res, next) => {
-  try {
-    const {id} = req.params;
-    const existingDog = await Dog.findByPk(id);
-    if(!existingDog) {
-      res.status(404).send(`Dog with id ${id} not found`);
-      return;
-    }
-    await Dog.destroy({where: {id}});
-    res.send(`deleted dog with id ${id}`);
-  } catch (error) {
-    next(error);
-  }
-});
-
-app.get('/dogs', async (req, res, next) => {
-  try {
-    const where = {};
-    for(const key of ['name', 'description', 'breed', 'color']) {
-      if(req.query[key]) {
-        where[key] =  {
-          [Op.like]: `%${req.query[key]}%` // search within the string, not only exact matches
-        };
-      }
-    }
-        
-    const dogs = await Dog.findAll({
-      where
-    });
-    res.send(dogs);
+    res.send('GET REQUEST HERE');
   } catch (error) {
     next(error)
+  }
+});
+
+app.post('', async (req, res, next) => {
+  try {
+   res.send('POST REQUEST HERE')
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('', async (req, res, next) => {
+  try {
+    res.send('DELETE REQUEST HERE')
+  } catch (error) {
+    next(error);
   }
 });
 
